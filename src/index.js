7e6153c7aae023688c7f40bf0005dd4b7a4e8cd5
Kalyan Jana <79560709+KalyanJana@users.noreply.github.com>
2023-07-18 00:31:16 +0530

    Server path changes for production
    Server path changes in index.js file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 // export const server ="http://localhost:4000"
-export const server ="https://kanbana-apps.onrender.com/"
+export const server ="https://kanbana-apps.onrender.com"
 
 export const Context = createContext()
 
